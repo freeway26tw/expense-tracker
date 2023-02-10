b08fc69575f4b87bc8b92af6d615c3ced468b35d
freeway26tw <freeway26tw@gmail.com>
2023-02-10 15:00:14 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -17,8 +18,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(3000, () => {
