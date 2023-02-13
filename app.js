78dcd547cd5b2383f581ac53b4ece56f0bd9ef74
freeway26tw <freeway26tw@gmail.com>
2023-02-13 12:22:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -7,24 +7,13 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 const moment = require('moment/moment')
 
+require('./config/mongoose')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express()
-
-// const db = mongoose.createConnection(process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', (error) => {
-  console.error('database error', error)
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs.engine({
   defaultLayout: 'main', extname: '.hbs',
@@ -37,10 +26,14 @@ app.engine('hbs', exphbs.engine({
 }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(methodOverride('_method'))
-
 app.use(routes)
 
 app.listen(3000, () => {
