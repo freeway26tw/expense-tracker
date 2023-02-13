104d1b2d764117c304ac5fd15d5b139b058bf915
freeway26tw <freeway26tw@gmail.com>
2023-02-13 10:16:54 +0800

    feat: revise user model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,14 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 })
 userSchema.plugin(autoIncrement, { id: 'user_id_counter', inc_field: '_id' })
