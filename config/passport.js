77e0654e0bf025e857d774a71443da4f30808278
freeway26tw <freeway26tw@gmail.com>
2023-02-13 20:33:51 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 
@@ -12,10 +13,13 @@ module.exports = app => {
         if (!user) {
           return done(null, false, req.flash('warning_msg', 'Email尚未註冊'))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('warning_msg', '帳號或密碼錯誤'))
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('warning_msg', '帳號或密碼錯誤'))
+            }
+            return done(null, user)
+          })
       })
       .catch(err => done(err, false))
   }))
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,8 +1,9 @@
 const User = require('../user')
 const Record = require('../record')
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
 
-const USER_SEED_DATA = { name: '廣志' }
+const USER_SEED_DATA = { name: '廣志', email: 'user1@example.com', password: '123' }
 
 const RECORD_SEED_DATA = [{ name: '午餐', date: '2019.4.23', amount: 60, categoryId: 4 }];
 
@@ -11,19 +12,29 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 db.once('open', () => {
-  User.create(USER_SEED_DATA)
-    .then(user => {
-      const userId = user
-      return Promise.all(Array.from(
-        { length: RECORD_SEED_DATA.length },
-        (_, i) => Record.create({ ...RECORD_SEED_DATA[i], userId })
-      ))
+
+  // 將使用者的密碼bcrypt處理
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(USER_SEED_DATA.password, salt))
+    .then(hash => {
+      USER_SEED_DATA.password = hash
     })
-    .then(() => {
-      console.log('done')
-      process.exit()
-    })
-    .catch(error => {
-      console.log(error)
+    .then(() => { // 此時USER_SEED_DATA的password已經hash了
+      User.create(USER_SEED_DATA)
+        .then(user => {
+          const userId = user
+          return Promise.all(Array.from(
+            { length: RECORD_SEED_DATA.length },
+            (_, i) => Record.create({ ...RECORD_SEED_DATA[i], userId })
+          ))
+        })
+        .then(() => {
+          console.log('done')
+          process.exit()
+        })
+        .catch(error => {
+          console.log(error)
+        })
     })
 })
