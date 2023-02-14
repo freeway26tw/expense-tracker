68b2bebe3a2eafe7eb25c15864002d7ac2b97b3f
freeway26tw <freeway26tw@gmail.com>
2023-02-14 09:20:27 +0800

    feat: registration validation

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
 
 const router = express.Router()
 
@@ -20,50 +21,53 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-
-  const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填' })
-  }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符' })
-  }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
+router.post('/register',
+  [
+    check('email').trim().notEmpty().withMessage('請輸入你的Email').isEmail().withMessage('請輸入有效的Email'),
+    check('password').trim().isLength({ min: 8 }).withMessage('密碼長度不可小於8個位元').isString().withMessage('請輸入有效的密碼'),
+    check('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('密碼和確認密碼不相符')
+      }
+      return true
     })
-  }
-
-  User.findOne({ email, platform: 'manual' }).then(user => {
-    if (user) {
-      errors.push({ message: '這個 Email 已經註冊過了' })
-      res.render('register', {
-        errors,
+  ],
+  (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).render('register', {
+        errorMessages: errors.array(),
         name,
         email,
         password,
         confirmPassword
       })
     }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash,
-        platform: 'manual'
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
+    User.findOne({ email, platform: 'manual' }).then(user => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了' })
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash,
+          platform: 'manual'
+        }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
   })
-})
 
 router.get('/logout', (req, res) => {
   req.logout(function (err) {
